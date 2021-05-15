3a592f0b2ecacac623c15591756f036558578d52
quillasp <saralfddin93@yahoo.fr>
2021-05-15 18:35:46 +0200

    Addition of the go fortunes server delivering JSON fortunes

--- step 2/README.md
+++ step 2/README.md
@@ -1,7 +1,8 @@
 # Etape 2
 
-Pour cette étape nous avons décidé d'utiliser **GoLang** comme serveur dynamique.
-C'était pour nous deux l'occasion de tester ce langage qui est plutôt simple à prendre en main et de plus en plus utilisé.
+Pour cette étape nous avons décidé d'utiliser **GoLang** comme serveur
+dynamique. C'était pour nous deux l'occasion de tester ce langage qui est plutôt
+simple à prendre en main et de plus en plus utilisé.
 
 Ces tutoriels nous ont été très utiles: 
 - [Apprentissage de GoLang](https://tour.golang.org/welcome/1)
@@ -9,37 +10,36 @@ Ces tutoriels nous ont été très utiles:
 
 ## Code GO
 
-Le code utilisé est le suivant:
-```go
-
-// REMPLIR ICI
-
-```
-
-TODO: résumer le code
-
+Pour faire la lecture du fichier `fortunes`, nous avons utilisé
+[ce repo](https://github.com/bmc/fortune-go) car le parsing était déjà effectué.
 
 ## Configuration
 
-Nous travaillons dans le répertoire `/usr/local/go/src/app`, qui est un sous-dossier du répertoire `$GOROOT`. 
-Sans cela, il ne serait pas possible de compiler l'application.
+Nous travaillons dans le répertoire `/usr/local/go/src/app`, qui est un
+sous-dossier du répertoire `$GOROOT`. Sans cela, il ne serait pas possible de
+compiler l'application.
 
 Il faut premièrement initialiser un module avec la commande `go mod init <nom>`
 
-Après avoir copié le fichier *.go* dans le répertoire courant, il faut installer les dépendances avec `run get` 
+Après avoir copié le fichier *.go* dans le répertoire courant, il faut installer
+les dépendances avec `run get` 
 
 On peut ensuite compiler et installer l'application avec `go install`
 
 La commande pour exécuter l'application est : `go run <nom>`
 
 ## Dockerfile
-L'image Docker utilisée est l'image officielle de GoLang sur Dockerhub: https://hub.docker.com/_/golang
 
-Toutes les manipulations de configuration décrites ci-dessus sont exécutées dans le dockerfile.
+L'image Docker utilisée est l'image officielle de GoLang sur Dockerhub:
+https://hub.docker.com/_/golang
+
+Toutes les manipulations de configuration décrites ci-dessus sont exécutées dans
+le dockerfile.
 
 Pour exécuter le dockerfile, c'est la même chose que d'habitude, mais attention:
-- Ctrl+C ne stoppe pas le container, alors il vaut mieux run le containeravec l'option `--detach` et stopper manuellement avec `docker stop <image>`
+- Ctrl+C ne stoppe pas le container, alors il vaut mieux run le containeravec
+  l'option `--detach` et stopper manuellement avec `docker stop <image>`
 
 ## Test
 
-TODO : À remplir
+![test](../figures/s2_test.png)
--- step 2/src/main.go
+++ step 2/src/main.go
@@ -2,41 +2,81 @@ package main
 
 import (
     "encoding/json"
+    "fmt"
+    "github.com/gorilla/mux"
+    "io/ioutil"
     "log"
+    "math/rand"
     "net/http"
+    "strings"
+    "time"
 )
 
 type Fortune struct {
     Title string `json:"Title"`
-    Desc string `json:"desc"`
     Content string `json:"content"`
 }
 
 var msg []Fortune
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    //m := Fortune
-    json.NewEncoder(w).Encode(msg)
+// Code repris de https://github.com/bmc/fortune-go
+// Pour les parties de lecture et parsing du fichier fortunes
 
+// Given a path representing a fortune file, load the file, parse it,
+// an return an array of fortune strings.
+func readFortuneFile(fortuneFile string) ([]string, error) {
+    content, err := ioutil.ReadFile(fortuneFile)
+    var fortunes []string = nil
+    if err == nil {
+        fortunes = strings.Split(string(content), "\n%\n")
+    }
+    return fortunes, err
+}
+
+// Given a path representing a fortune file, load the file, parse it,
+// choose a random fortune, and display it to standard output. Returns
+// a Go error object on error or nil on success.
+func findAndPrint(fortuneFile string) error {
+    msg = make([]Fortune, 0)
+    fortunes, err := readFortuneFile(fortuneFile)
+
+    if err == nil {
+        rand.Seed(time.Now().UTC().UnixNano())
+        min := 1
+        max := 10
+        r := rand.Intn(max - min + 1) + min
+
+        for i := 0; i < r; i++ {
+            rand.Seed(time.Now().UTC().UnixNano())
+            f := rand.Int() % len(fortunes)
+            title := fmt.Sprintf("fortune %d", i + 1)
+            msg = append(msg, Fortune{Title: title,Content: fortunes[f]})
+        }
+    }
+    fmt.Sprintln("message length : ", len(msg))
+    return err
+}
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    errf := findAndPrint("fortunes")
+    if errf != nil { errf.Error() }
+
+    enc := json.NewEncoder(w)
+    enc.SetEscapeHTML(true)
+    enc.SetIndent("", "    ")
+
+    errj := enc.Encode(msg)
+    if errj != nil { errj.Error() }
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homePage)//.Methods("GET")
+    r := mux.NewRouter()
+    r.HandleFunc("/", homePage).Methods("GET")
+    http.Handle("/", r)
     log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func main() {
-    msg =  []Fortune{
-        Fortune{
-            Title:   "In the future",
-            Desc:    "You will die some day",
-            Content: "Yes you will",
-        },
-        Fortune{
-            Title:   "Today",
-            Desc:    "You have eaten",
-            Content: "Am I right ?",
-        },
-    }
+    fmt.Println("Server is running on port 80")
     handleRequests()
 }
