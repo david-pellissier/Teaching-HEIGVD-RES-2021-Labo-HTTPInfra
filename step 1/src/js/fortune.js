6c17d8503021a356f5e84b3c5773ae77d9d9669c
david-pellissier <dav.pellissier@gmail.com>
2021-05-21 16:37:32 +0200

    MaJ http

---
+++ step 1/src/js/fortune.js
@@ -0,0 +1,28 @@
+$(
+    function(){
+        console.log("loading.....");
+
+
+        function loadFortune() {
+
+            $.getJSON("/api/fortunes/", function ( fortunes) {
+
+                console.log(fortunes);
+
+                let message = "No luck for you today...";
+
+                if(fortunes.length > 0) {
+                        message  = fortunes[0].content
+
+                }
+
+                $("#message").text = message;
+            }
+            );
+
+        }
+
+        loadFortune();
+
+    }
+)
