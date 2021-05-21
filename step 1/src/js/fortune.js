15924176c227a9413707e747e876d5d4ba41d1b2
david-pellissier <dav.pellissier@gmail.com>
2021-05-21 17:19:41 +0200

    MàJ page HTML

--- step 1/src/js/fortune.js
+++ step 1/src/js/fortune.js
@@ -1,28 +1,25 @@
-$(
-    function(){
+$(document).ready(function() {
         console.log("loading.....");
 
 
         function loadFortune() {
 
-            $.getJSON("/api/fortunes/", function ( fortunes) {
+            $.getJSON("/api/fortunes/", function (fortunes) {
 
-                console.log(fortunes);
+                    console.log(fortunes);
 
-                let message = "No luck for you today...";
+                    let message = "No luck for you today...";
 
-                if(fortunes.length > 0) {
-                        message  = fortunes[0].content
+                    if (fortunes.length > 0) {
+                        message = fortunes[0].content
 
+                    }
+
+                    $("#message").text(message);
                 }
-
-                $("#message").text = message;
-            }
             );
 
         }
 
         loadFortune();
-
-    }
-)
+});
